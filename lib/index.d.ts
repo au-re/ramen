11f7d014f64f1b657631386a0a9e03f9addd39a8
Aurélien <au.re@live.com>
2019-11-08 17:11:49 +0100

    :fire: (lib) remove lib folder

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Editor } from "./components/Editor/Editor";
-export { default as Node } from "./components/Node/Node";
-export { default as Noodle } from "./components/Noodle/Noodle";
-export { default as Pin } from "./components/Pin/Pin";
